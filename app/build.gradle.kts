eef150a883311ea3608afa48c6f5fa7a4b5bbda9
abhishekdongle <abhidongle04@gmail.com>
2025-04-12 04:53:56 +0530

    dev: create ArticlesService with HttpClient to fetch API response

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -54,6 +55,10 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.navigation.compose)
     implementation(libs.androidx.material.icons.extended)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/abhishek/dongle/newsapp/article/ArticlesViewModel.kt
+++ app/src/main/java/com/abhishek/dongle/newsapp/article/ArticlesViewModel.kt
@@ -1,63 +1,49 @@
 package com.abhishek.dongle.newsapp.article
 
+import android.util.Log
 import com.abhishek.dongle.newsapp.base.BaseViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 
 class ArticlesViewModel : BaseViewModel() {
 
     private val _articleState = MutableStateFlow(ArticlesState(loading = true))
     val articleState: StateFlow<ArticlesState> get() = _articleState
 
+    private var httpClient: HttpClient
+    private var articlesService: ArticlesService
+
     init {
+        httpClient = getHttpClient()
+        articlesService = ArticlesService(httpClient)
         getArticles()
     }
 
     private fun getArticles() {
         scope.launch {
-            val fetchedArticles = fetchArticles()
+            val fetchedArticles = articlesService.fetchArticles()
             delay(1000)
             _articleState.emit(ArticlesState(error = "Something went wrong."))
-
-            delay(1000)
-            _articleState.emit(ArticlesState(articles = fetchedArticles))
+            Log.d("test: ", "list = ${fetchedArticles.size}")
+//            _articleState.emit(ArticlesState(articles = fetchedArticles))
         }
     }
 
-    private suspend fun fetchArticles() = mockArticle
-
-    private val mockArticle = listOf(
-        Article(
-            title = "Bitcoin Prediction - Crypto Market for BINANCE:BTCUSDT.P by UA_CAPITAL",
-            desc = "Bitcoin has broken the weekly structure to the downside, and we've been bearish since then. However, the monthly chart still looks bullish, and I expect the price to return to the monthly demand zone and get a reaction from there. I believe the crypto market could turn bullish again by the end of ...",
-            date = "2025-04-10",
-            imageUrl = "https://s3.tradingview.com/g/GkAsIJWO_big.png"
-        ),
-        Article(
-            title = "How to Recover Stolen Cryptocurrency with Proven Methods",
-            desc = "Stolen cryptocurrency can be recovered. The need to recover stolen crypto has never been more urgent. Crypto thieves have already stolen approximately $730 million through different scams in the first two months of 2025 alone. These thefts account for one-third of all crypto heists from last ...",
-            date = "2025-04-10",
-            imageUrl = "https://assets.infosecurity-magazine.com/webpage/tw/605e115e-d012-4227-aa8b-df499239a604.jpg"
-        ),
-        Article(
-            title = "Warren Buffett Holds Record $334 Billion in Cash -- And Why He's Still Snubbing Bitcoin",
-            desc = "Investigations suggest Berkshire's anti-Bitcoin stance may stem from business interests tied to natural gas, not just ideology. Warren Buffett, Chairman and CEO of Berkshire Hathaway, has once again drawn global attention. His firm now holds a record-breaking $334 billion cash balance -- the ...",
-            date = "2025-04-10",
-            imageUrl = "https://beincrypto.com/wp-content/uploads/2023/10/BIC_Warren_Buffet-covers_positive.png.webp"
-        ),
-        Article(
-            title = "BIT Mining Announces March 2025 Production and Operations Updates",
-            desc = "AKRON, Ohio, April 10, 2025 /PRNewswire/ -- BIT Mining Limited (NYSE: BTCM) (\"BIT Mining\" or the \"Company\"), a leading technology-driven cryptocurrency mining company, today released its monthly mining production and operational updates for March 2025. BIT Mining continues to expand its ...",
-            date = "2025-04-10",
-            imageUrl = "https://greenstocknews.com/images/ogimage.jpg"
-        ),
-        Article(
-            title = "Sandow Lakes' natural gas power plant location was its selling point. Until it wasn't.",
-            desc = "Many cheered when Sandow Lakes Energy first announced plans to build a huge new natural gas power plant in a region within the Texas Triangle still reeling from the loss of a major industrial complex. Gov. Greg Abbott and Texas lawmakers touted the new power plant's potential to supply more ...",
-            date = "2025-04-10",
-            imageUrl = "https://s.yimg.com/cv/apiv2/social/images/yahoo_default_logo-1200x1200.png"
-        )
-    )
+    private fun getHttpClient(): HttpClient {
+        val client = HttpClient(OkHttp) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+        return client
+    }
 }
