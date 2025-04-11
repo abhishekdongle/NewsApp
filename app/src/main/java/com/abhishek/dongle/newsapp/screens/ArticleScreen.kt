afae58d7306ab4a2ea445b3a71b5881c39e29ac4
abhishekdongle <abhidongle04@gmail.com>
2025-04-11 13:05:04 +0530

    dev: added MoreVert option in toolbar

--- app/src/main/java/com/abhishek/dongle/newsapp/screens/ArticleScreen.kt
+++ app/src/main/java/com/abhishek/dongle/newsapp/screens/ArticleScreen.kt
@@ -10,8 +10,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -33,11 +37,11 @@ fun ArticlesScreen(articleViewModel: ArticlesViewModel) {
 
     Column {
         AppBar()
-        if(articleState.value.loading)
+        if (articleState.value.loading)
             Loader()
-        if(articleState.value.error != null)
+        if (articleState.value.error != null)
             ErrorMessage(articleState.value.error!!)
-        if(articleState.value.articles.isNotEmpty())
+        if (articleState.value.articles.isNotEmpty())
             ArticleListView(articleState.value.articles)
     }
 }
@@ -46,7 +50,15 @@ fun ArticlesScreen(articleViewModel: ArticlesViewModel) {
 @Composable
 private fun AppBar() {
     TopAppBar(
-        title = { Text(text = "Articles")}
+        title = { Text(text = "Articles") },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Outlined.MoreVert,
+                    contentDescription = "Setting Button"
+                )
+            }
+        }
     )
 }
 
@@ -61,9 +73,10 @@ fun ArticleListView(articles: List<Article>) {
 
 @Composable
 fun ArticleItemView(article: Article) {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
     ) {
         AsyncImage(
             model = article.imageUrl,
